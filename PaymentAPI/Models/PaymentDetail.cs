6e4a5abd7002314784470cca66785cdc6a81e89d
DivanS <DivanS@bellequipment.com>
2024-08-27 09:49:26 +0200

    Add project files.

---
+++ PaymentAPI/Models/PaymentDetail.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetailId { get; set; }
+        public int CardOwnerName  { get; set; }
+        public int CardNumber  { get; set; }
+        public int ExpirationDate  { get; set; }
+        public int SecurityCode  { get; set; }
+    }
+}
